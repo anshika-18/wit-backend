f4ab7704e2831309e5e7fd7482db5d073f39ba92
Anjali <anjali170601@gmail.com>
2022-02-11 22:53:38 +0530

    Login route added

--- route/auth.js
+++ route/auth.js
@@ -44,6 +44,37 @@ module.exports = (app) => {
             return res.status(500).json(err);
         }
     });
+
+    app.post('/login', [
+        body('email', 'Enter a valid email').isEmail(),
+        body('password', 'Password cannot be blank').exists(),
+      ], async (req, res) => {
+        let success = false;
+        // If there are errors, return Bad request and the errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+        }
+      
+        const { emailId, password } = req.body;
+        try {
+          let user = await User.findOne({ emailId });
+          if (!user) {
+            success = false
+            return res.status(400).json({ error: "Please try to login with correct credentials" });
+          }
+      
+          const passwordCompare = await bcrypt.compare(password, user.password);
+          if (!passwordCompare) {
+            success = false
+            return res.status(400).json({ success, error: "Please try to login with correct credentials" });
+          }
+
+        } catch (error) {
+          console.error(error.message);
+          res.status(500).send("Internal Server Error");
+        }
+      });
 }
 
 
